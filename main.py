9fa37b0c48ebf4f649bbc1cb38d0435b5eeb1de2
LoganK313 <kinglogan3131@gmail.com>
2026-01-15 13:04:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def output_dice(dice):
     pass
 
 def roll_die():
-    # accepts nothign
+    # accepts nothing
     # returns a random iteger between 1 and 6
     return randint(1,7)
 
 def first_roll():
-    pass
+    # accepts nothing
+    # returns the random integers between 1 and 12
+    list = [roll_die() for num in range(13)]
+    print(list)
 
 def count_frequencey(dice, number):
     pass
