c32f8338c0a32316357ec5c3dab424bf005fae71
LoganK313 <kinglogan3131@gmail.com>
2026-01-15 12:44:10 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    pass
+
+def output_dice(dice):
+    pass
+
+def roll_die():
+    pass
+
+def first_roll():
+    pass
+
+def count_frequencey(dice, number):
+    pass
+
+def find_mode():
+    pass
+
+def list_unmatched_dice(dice):
+    pass
+
+def reroll_one(dice, index):
+    pass
+
+def reroll_many(dice):
+    pass
